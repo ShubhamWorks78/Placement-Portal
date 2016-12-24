1cf9be9f10a76dedfb8fe7c01f8ae8f191aa4fdb
Shubham <2014UGEC078@nitjsr.ac.in>
2016-12-24 22:12:40 +0530

    Home Screen Aka Splash Activity added

--- app/src/main/java/com/seals/shubham/myapplication/HomeActivity.java
+++ app/src/main/java/com/seals/shubham/myapplication/HomeActivity.java
@@ -1,13 +1,24 @@
 package com.seals.shubham.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Handler hnd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        hnd = new Handler();
+        hnd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),LoginActivity.class);
+                HomeActivity.this.startActivity(i);
+                HomeActivity.this.finish();
+            }
+        },5000);
     }
 }
